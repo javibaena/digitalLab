58adc16d2b39c6ccd523b9ff6e8707f38cd1829b
JaviBaena <javibaenatro15@gmail.com>
2023-05-30 22:53:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+/* flickity */
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  cellAlign: 'left',
+  contain: true,
+  autoPlay: true,
+  wrapAround:true,
+  freeScroll: true,
+  contain: true,
+// disable previous & next buttons and dots
+   prevNextButtons: false,
+   pageDots: true,
+});
+/* fin flickiy */
+
+/* cerrar menu */
+var menuHamburguesa = document.querySelector(".menu-hamburguesa");
+var cajaMenuHamburguesa = document.querySelector(".menu-desplegable-header");
+var cierre= document.querySelector('.close');
+var cierreCruz= document.querySelector('.closeDos');
+
+
+menuHamburguesa.addEventListener('click', function (){
+  cajaMenuHamburguesa.classList.add('visible');
+  
+});
+cierre.addEventListener('click', function(){
+  cajaMenuHamburguesa.classList.remove('visible');
+});
+cierreCruz.addEventListener('click', function(){
+  cajaMenuHamburguesa.classList.remove('visible');
+});
+
